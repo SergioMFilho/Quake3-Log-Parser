85272a69bbd1fe264fcb5afbeaf48da0c8e2cdff
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-17 15:10:07 -0300

    Reading line by line

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-
-const fs = require('fs')
-
-const nome_arquivo = '/Quake.txt'
-
-const path = __dirname + `${nome_arquivo}`
-
-function meuLeitorDeArquivo(caminho) {
-    fs.readFile(caminho, 'utf-8', function(error,data) {
-        if(error){
-            console.log('erro de leitura: '+ error.message)
-        } else {
-            console.log(data)
-        }
-    })
-}
+const fs = require('fs');
+const readline = require('readline');
+async function readFileByLine(file) {
+  const fileStream = fs.createReadStream(file);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  for await (const line of rl) {
+    console.log(">>>" + line);
+  }
+ }
+ 
+readFileByLine('Quake.txt')
 
 
-meuLeitorDeArquivo(path)
+// const fs = require('fs')
+
+// const nome_arquivo = '/Quake.txt'
+
+// const path = __dirname + `${nome_arquivo}`
+
+// function meuLeitorDeArquivo(caminho) {
+//     fs.readFile(caminho, 'utf-8', function(error,data) {
+//         if(error){
+//             console.log('erro de leitura: '+ error.message)
+//         } else {
+//             console.log(data)
+//         }
+//     })
+// }
+
+// meuLeitorDeArquivo(path)
 
 
